ecd6de56e1a78ab6f3e54ea5b0c7cac2223e56c1
criley <chris.riley@cprime.com>
2020-03-04 11:14:24 -0500

    Initial Commit

---
+++ SimpleConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SimpleMathClassLibrary;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Console Calculator");
+            Calculator calc = new Calculator();
+            int result = calc.addition(10, 20);
+            Console.WriteLine($"Addition of 10 and 20 = {result}");
+        }
+    }
+}
---
+++ SimpleMathClassLibrary/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleMathClassLibrary
+{
+    public class Calculator
+    {
+       public int addition(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+
+    }
+}
